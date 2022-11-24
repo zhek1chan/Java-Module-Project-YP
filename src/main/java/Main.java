aca4b95b73b82c088c91dd074ad874a1222d8654
sergeybuvaka <s.buvaka@fhl.world>
2022-11-24 22:57:32 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // ваш код начнется здесь
+        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
+        System.out.println("Привет Мир");
+    }
+}
